971c8cdd304bff6c943f0253eb36c46c654d22b7
genius <csj4032@gmail.com>
2015-07-21 09:50:41 +0900

    TemplateMethodMain 추가

--- src/com/pearl/genius/templateMethod/TemplateMethodMain.java
+++ src/com/pearl/genius/templateMethod/TemplateMethodMain.java
@@ -1,12 +1,15 @@
 package com.pearl.genius.templateMethod;
 
 /**
+ * 상위 클래스쪽에 템플릿에 해당하는 메소드가 정의되어 있고 그 메소드의 정의 안에는 추상 메소드 가 시용되고 있음
+ * 상위 클래스의 프로그램만 보면 추상 메소드를 어떻게 호출 하고 있는지 알 수 있지만 최종적으로 어떤 처리가 수행되는지는 알 수 없음
+ *
  * 템플릿이란 문자 모양으로 구멍이 뚫려있는 앓은 플라스틱 판을 말합니다.
  * 그 구멍을 따라 펜으로 그리면 손으로도 반듯한 문자를 쓸 수 있습니다.
  * 템플릿의 구멍을 보면 어떤 모양의 문자인지는 알 수 있지만, 실제로 어떤 문자가 될지는 필기구에 의해 결정됩니다.
  */
 
-public class TempMain {
+public class TemplateMethodMain {
 
 	public static void main(String[] args) {
 		AbstractDisplay d1 = new CharDisplay('H');
