edcd59ca7a0bcae028c56d9e33ec42955b6464a8
unknown <csj4032@gmail.com>
2015-07-28 02:54:27 +0900

    Builder

--- src/com/pearl/genius/builder/HTMLBuilder.java
+++ src/com/pearl/genius/builder/HTMLBuilder.java
@@ -1,15 +1,27 @@
 package com.pearl.genius.builder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by Administrator on 2015-07-28.
  */
 public class HTMLBuilder extends Builder {
 
-	private StringBuffer buffer = new StringBuffer();
+	private String filename;
+	private PrintWriter writer;
 
 	@Override
 	public void makeTitle(String title) {
-
+		filename = title + ".html";
+		try {
+			writer = new PrintWriter(new FileWriter(filename));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		writer.println("<html><head><title>"+title+"</title></head><body>");
+		writer.println("<h1>"+title+"</h1>");
 	}
 
 	@Override
@@ -24,10 +36,11 @@ public class HTMLBuilder extends Builder {
 
 	@Override
 	public void close() {
-
+		writer.println("</body></html>");
+		writer.close();
 	}
 
-	public StringBuffer getResult() {
-		return buffer;
+	public String getResult() {
+		return filename;
 	}
 }
--- src/com/pearl/genius/builder/TextBuilder.java
+++ src/com/pearl/genius/builder/TextBuilder.java
@@ -9,22 +9,27 @@ public class TextBuilder extends Builder {
 
 	@Override
 	public void makeTitle(String title) {
-
+		buffer.append("=============================\n");
+		buffer.append("『" + title + "』");
+		buffer.append("\n");
 	}
 
 	@Override
-	public void makeString(String title) {
-
+	public void makeString(String str) {
+		buffer.append("■" + str + "\n");
+		buffer.append("\n");
 	}
 
 	@Override
-	public void makeItems(String title) {
-
+	public void makeItems(String[] items) {
+		for (int i = 0; i < items.length; i++) {
+			buffer.append("＊" + items[i] + "\n");
+		}
 	}
 
 	@Override
 	public void close() {
-
+		buffer.append("=============================\n");
 	}
 
 	public String getResult() {
