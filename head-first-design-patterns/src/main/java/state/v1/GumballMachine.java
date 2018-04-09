f596c188cdcf27bf2cc6066abb048a69642a4a66
genius.choi <csj4032@gmail.com>
2018-04-09 23:47:10 +0900

    State pattern

---
+++ head-first-design-patterns/src/main/java/state/v1/GumballMachine.java
@@ -0,0 +1,57 @@
+package state.v1;
+
+public class GumballMachine {
+
+	private final static int SOLD_OUT = 0;
+	private final static int NO_QUARTER = 1;
+	private final static int HAS_QUARTER = 2;
+	private final static int SOLD = 3;
+	private int count;
+
+	int state = SOLD_OUT;
+
+	public GumballMachine(int count) {
+		count = count;
+		if (count > 0) {
+			state = NO_QUARTER;
+		}
+	}
+
+	public void insertQuarter() {
+		if (state == HAS_QUARTER) {
+			System.out.println("동전은 한 개만 넣어주세요");
+		} else if (state == SOLD_OUT) {
+			System.out.println("매진되었습니다. 다음 기회에 이용해주세요");
+		} else if (state == SOLD) {
+			System.out.println("잠깐만 기다려 주세요. 알맹이가 배출되고 있습니다.");
+		} else if (state == NO_QUARTER) {
+			state = HAS_QUARTER;
+			System.out.println("동전이 투입되었습니다.");
+		}
+	}
+
+	public void ejectQuarter() {
+		if (state == HAS_QUARTER) {
+			System.out.println("동전이 반환됩니다.");
+		}
+	}
+
+	public void turnCrank() {
+		if (state == SOLD) {
+			System.out.println("손잡이는 한번만 돌려주세요");
+		}
+	}
+
+	public void dispense() {
+		if (state == SOLD) {
+			System.out.println("알맹이가 나가고 있습니다.");
+			count = count - 1;
+			if (count == 0) {
+				System.out.println("더 이상 알맹이가 없습니다.");
+				state = SOLD_OUT;
+			} else {
+				state = NO_QUARTER;
+			}
+		}
+	}
+}
