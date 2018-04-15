2b2264762b240c52fa4d56c8a12140c97233fb88
genius.choi <csj4032@gmail.com>
2018-04-16 00:45:22 +0900

    Flyweight pattern

--- yuki-hiroshi-design-patterns/src/main/java/com/pearl/genius/flyweight/BigCharFactory.java
+++ yuki-hiroshi-design-patterns/src/main/java/com/pearl/genius/flyweight/BigCharFactory.java
@@ -16,10 +16,21 @@ public class BigCharFactory {
 	}
 
 	public BigChar getBigChar(char charName) {
+
+//		BigChar bc = pool.get(charName);
+//		if (bc == null) {
+//			bc = new BigChar(charName);
+//			pool.put(charName, bc);
+//		}
+//		return bc;
+
 		//If the specified key is not already associated with a value (or is mapped to null) associates it with the given value and returns null, else returns the current value.
 		//return pool.putIfAbsent(charName, new BigChar(charName));
 
 		//If the specified key is not already associated with a value (or is mapped to null), attempts to compute its value using the given mapping function and enters it into this map unless null.
 		return pool.computeIfAbsent(charName, k -> new BigChar(k));
+
+		//https://docs.oracle.com/javase/tutorial/java/javaOO/methodreferences.html
+		//return pool.computeIfAbsent(charName, BigChar::new);
 	}
 }
