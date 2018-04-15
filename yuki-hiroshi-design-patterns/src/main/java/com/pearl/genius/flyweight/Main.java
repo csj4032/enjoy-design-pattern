1f0866fc1f633b35b33c969fe5639154a888d776
genius.choi <csj4032@gmail.com>
2018-04-15 20:50:36 +0900

    Flyweight pattern

--- yuki-hiroshi-design-patterns/src/main/java/com/pearl/genius/flyweight/Main.java
+++ yuki-hiroshi-design-patterns/src/main/java/com/pearl/genius/flyweight/Main.java
@@ -3,12 +3,7 @@ package com.pearl.genius.flyweight;
 public class Main {
 
 	public static void main(String[] args) {
-//		if (args.length == 0) {
-//			System.out.println("Usage : Java Main digits");
-//			System.out.println("Example : Java Main 1212123");
-//			System.exit(0);
-//		}
-		BigString bs = new BigString("0123456789");
+		BigString bs = new BigString("010-3429-7530");
 		bs.print();
 	}
 }
